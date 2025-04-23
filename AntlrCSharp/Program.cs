d0901ed80455265b17b43237a1805674db824e23
ThomasBow <101808093+ThomasBow@users.noreply.github.com>
2025-04-23 22:01:14 +0200

    Update Program.cs
    Added boiler plate
    - needs to get the correct input

--- AntlrCSharp/Program.cs
+++ AntlrCSharp/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+
+using Antlr4.Runtime;
+
+try
+{
+    string input = "";
+
+    AntlrInputStream inputStream = new(input);
+    AQLLexer lexer = new(inputStream);
+    CommonTokenStream commonTokenStream = new(lexer);
+    AQLParser parser = new(commonTokenStream);
+
+    AQLParser.ProgContext progContext = parser.prog();
+    BasicAQLVisitor visitor = new();
+    object result = visitor.Visit(progContext);
+
+    Console.WriteLine(result.ToString());
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
