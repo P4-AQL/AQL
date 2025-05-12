6e63629514cd80d2bd85839394e76e4babd0ce14
Patrick <patrick@reiffenste.in>
2025-05-12 12:03:36 +0200

    New stuff

--- Interpreter/Semantic Analysis/Environment.cs
+++ Interpreter/Semantic Analysis/Environment.cs
@@ -1,10 +1,23 @@
-
-
-
 using Interpreter.AST.Nodes;
+using Interpreter.AST.Nodes.NonTerminals;
 
 namespace Interpreter.SemanticAnalysis;
 public class Environment
 {
-    Dictionary<string, object> VTable = [];
+    // Identifier -> type
+    private readonly Dictionary<string, TypeNode> Table = [];
+    private void Bind(string identifier, TypeNode nodeType) => Table.Add(identifier, nodeType);
+
+    public bool LookUp(string id, out TypeNode? @out) => Table.TryGetValue(id, out @out);
+
+    // return true means success
+    public bool TryBind(string identifier, TypeNode typeNode) {
+        //Bind failed
+        if (LookUp(identifier, out TypeNode? _)) return false;
+        
+        //Bind succeeded
+        Bind(identifier, typeNode);
+        return true;
+
+    }
 }
