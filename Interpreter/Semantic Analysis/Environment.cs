0e40cc51295f40a21e27b15cab53a5f21aa01a8e
ThomasBow <101808093+ThomasBow@users.noreply.github.com>
2025-05-12 12:36:02 +0200

    Generic environment

--- Interpreter/Semantic Analysis/Environment.cs
+++ Interpreter/Semantic Analysis/Environment.cs
@@ -3,32 +3,28 @@ using Interpreter.AST.Nodes.Definitions;
 using Interpreter.AST.Nodes.NonTerminals;
 
 namespace Interpreter.SemanticAnalysis;
-public class Environment
+public class Environment<T>
 {
     // Identifier -> type
-    private readonly Dictionary<string, TypeNode> TypeTable = [];
-    Dictionary<string, FunctionNode> FunctionTable { get; } = [];
-
-    private void Bind<T>(string identifier, T @object, Dictionary<string, T> table) => table.Add(identifier, @object);
-    private void Bind(string identifier, TypeNode nodeType) => Bind(identifier, nodeType, TypeTable);
-    private void Bind(string identifier, FunctionNode nodeFunction) => Bind(identifier, nodeFunction, FunctionTable);
+    private readonly Dictionary<string, T> Table = [];
+    private void Bind(string identifier, T @object) => Table.Add(identifier, @object);
 
 
-    public bool LookUp(string id, out TypeNode? @out) => TypeTable.TryGetValue(id, out @out);
+    public bool LookUp(string id, out T? @out) => Table.TryGetValue(id, out @out);
 
     /// <summary>
     /// Binds a identifier to the type table, if it does not exist.
     /// </summary>
     /// <param name="identifier">The identifier to bind.</param>
-    /// <param name="typeNode">The type to bind.</param>
+    /// <param name="object">The object to bind.</param>
     /// <returns>True if the variable does not already exist; Otherwise false.</returns>
-    public bool TryBind(string identifier, TypeNode typeNode)
+    public bool TryBindIfNotExists(string identifier, T @object)
     {
         //Bind failed - variable already exists
-        if (LookUp(identifier, out TypeNode? _)) return false;
+        if (LookUp(identifier, out T? _)) return false;
 
         //Bind succeeded
-        Bind(identifier, typeNode);
+        Bind(identifier, @object);
         return true;
 
     }
